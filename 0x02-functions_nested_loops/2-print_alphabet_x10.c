702c436a30a80713b86029a590c6249362aeac22
Pussywagon08 <rilwanabioye08@gmail.com>
2023-03-22 01:06:34 +0100

    all-at-once

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet a-z * 10
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this directory contains my ans for 0x02-functions_nested_loops tasks
